36f58b47b5ba9c5435b60e976c61b803deff97b8
official16Dec <officialsunil16@gmail.com>
2025-07-31 04:52:39 +0530

    Home screen custom action bar configured.

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -13,6 +15,35 @@ class HomeScreen extends StatelessWidget {
               fit: BoxFit.cover, // Covers the entire screen
             ),
           ),
+
+        child: Column(
+          children: [
+            const SizedBox(height: 40),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const SizedBox(width: 20),
+                Image.asset(
+                  'assets/appIcon.png', // Replace with your icon path
+                  width: 60,
+                  height: 60,
+                ),
+                const SizedBox(width: 10),
+                Image.asset(
+                  'assets/muzicvia_title.png',
+                  height: 40,
+                ),
+              ],
+            ),
+            Divider(
+              color: Colors.white, // Color of the line
+              thickness: 1.0,     // Thickness of the line
+              height: 20.0,       // Height of the divider widget, including the line and any surrounding space.
+              indent: 16.0,       // Left padding of the line
+              endIndent: 16.0,    // Right padding of the line
+            )
+          ],
+        ),
       ),
     );
   }
